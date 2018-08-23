500c3da858dc6b61cedb43b06cab4badbc81d80d
buzenm <caochunke458@hotmail.com>
2018-08-23 15:46:28 +0800

    搞定半屏显示

--- YourDiary3/MainPage.xaml.cs
+++ YourDiary3/MainPage.xaml.cs
@@ -14,6 +14,11 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading;
+using System.Drawing;
+using Size = Windows.Foundation.Size;
+using System.Management;
+
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -37,12 +42,39 @@ namespace YourDiary3
 
         public static Size GetDeviceResolution()
         {
+            //using (Graphics graphics = Graphics.FromHwnd(IntPtr.Zero))
+            //{
+            //    float dpiX = graphics.DpiX;
+            //    float dpiY = graphics.DpiY;
+            //}
+
+            //using (ManagementClass mc = new ManagementClass("Win32_DesktopMonitor"))
+            //{
+            //    using (ManagementObjectCollection moc = mc.GetInstances())
+            //    {
+
+            //        int PixelsPerXLogicalInch = 0; // dpi for x
+            //        int PixelsPerYLogicalInch = 0; // dpi for y
+
+            //        foreach (ManagementObject each in moc)
+            //        {
+            //            PixelsPerXLogicalInch = int.Parse((each.Properties["PixelsPerXLogicalInch"].Value.ToString()));
+            //            PixelsPerYLogicalInch = int.Parse((each.Properties["PixelsPerYLogicalInch"].Value.ToString()));
+            //        }
+
+            //        Console.WriteLine("PixelsPerXLogicalInch:" + PixelsPerXLogicalInch.ToString());
+            //        Console.WriteLine("PixelsPerYLogicalInch:" + PixelsPerYLogicalInch.ToString());
+            //        Console.Read();
+            //    }
+            //}
             Size resolution = Size.Empty;
-            var rawPixelsPerViewPixel = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            //var rawPixelsPerViewPixel = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            //var DPI = DisplayInformation.GetForCurrentView().RawDpiX;
+
             foreach (var item in PointerDevice.GetPointerDevices())
             {
-                resolution.Width = item.ScreenRect.Width * rawPixelsPerViewPixel;
-                resolution.Height = item.ScreenRect.Height * rawPixelsPerViewPixel;
+                resolution.Width = item.ScreenRect.Width;
+                resolution.Height = item.ScreenRect.Height;
                 break;
             }
             return resolution;
